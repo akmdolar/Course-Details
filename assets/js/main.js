b231bea75efbe6bad595f0ed689c399a085d042c
akm dollar <akmdolar@outlook.com>
2020-06-27 19:41:35 +0600

    Course Details

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+$('.carousel').carousel({
+  interval: 5000
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: ['<i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i>','<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>'],
+     autoplay: true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+$(document).ready(function(){
+    $('.counter').counterUp({
+      delay: 10,
+      time: 2000
+    });
+    $('.counter').addClass('animated fadeInDownBig');
+    $('h3').addClass('animated fadeIn');
+});
